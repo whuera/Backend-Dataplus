c6ff7cfd560c121d7e58cc3c79af1abb511a17da
William Huera <whuera@gmail.com>
2017-11-23 16:35:42 -0500

    update artifact

---
+++ gs-accessing-data-mysql-complete/src/main/java/ec/com/app/bundle/backend/filter/AppFilter.java
@@ -0,0 +1,39 @@
+package ec.com.app.bundle.backend.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppFilter implements Filter{
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
+		response.setHeader("Access-Control-Allow-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Max-Headers", "x-requested-with, content-type");
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
