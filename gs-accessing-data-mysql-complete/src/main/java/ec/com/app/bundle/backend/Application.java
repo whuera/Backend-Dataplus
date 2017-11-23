166b797f9c47b70d26960b70fae13bb2d3dcc308
William Huera <whuera@gmail.com>
2017-11-23 15:02:07 -0500

    update artifact

--- gs-accessing-data-mysql-complete/src/main/java/ec/com/app/bundle/backend/Application.java
+++ gs-accessing-data-mysql-complete/src/main/java/ec/com/app/bundle/backend/Application.java
@@ -3,12 +3,19 @@ package ec.com.app.bundle.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "ec.com.app.bundle.backend.repository")
 @EntityScan(basePackages = "ec.com.app.bundle.backend.model")
-public class Application {
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
